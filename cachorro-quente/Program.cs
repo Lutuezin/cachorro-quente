c0a3980d98fb5718adbedb2e29b64c1536bd5aeb
ALUNO <ALUNO@LABFPC22>
2024-02-28 11:56:59 -0300

    Adicionar arquivos de projeto.

---
+++ cachorro-quente/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cachorro_quente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float h, p;
+            h = float.Parse(Console.ReadLine());
+            p = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(String.Format("{0:C2}", (h / p)));
+            Console.ReadKey();
+        }
+        
+    }
+}
